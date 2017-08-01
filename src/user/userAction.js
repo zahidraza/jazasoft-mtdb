65d0e24d93739069f1dcec3222298c38bd0ccb1a
Md Zahid Raza <zahid7292@ymail.com>
2017-08-01 20:09:57 +0530

    first commit

---
+++ src/user/userAction.js
@@ -0,0 +1,5 @@
+export const USER_CREATE = 'USER_CREATE';
+
+export function createUser (user) {
+  return {type: USER_CREATE};
+};
---
+++ src/user/userReducer.js
@@ -0,0 +1,15 @@
+import { USER_CREATE } from './userAction';
+
+const initialState = {
+  users: []
+};
+
+const handlers = { 
+  [USER_CREATE]: (_, action) => ({})
+};
+
+export default function testReducer (state = initialState, action) {
+  let handler = handlers[action.type];
+  if( !handler ) return state;
+  return { ...state, ...handler(state, action) };
+}
